30fa110eb05167dabb4018bdbaaa7651cef860e4
dvmoomoodv <dvmoomoodv@gmail.com>
2020-01-14 16:53:49 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyCUrv5r8SS3z7GbCxP177xQ8HdCj2uuU_I'
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
